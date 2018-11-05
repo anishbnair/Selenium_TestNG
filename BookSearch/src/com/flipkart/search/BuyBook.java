70f79b55bf449ee26ccd1b86b70fe074923dd8a9
anishbnair <nbanish@gmail.com>
2018-11-05 17:09:43 -0500

    added PreConditions method and assigned chromedriver to driver

--- BookSearch/src/com/flipkart/search/BuyBook.java
+++ BookSearch/src/com/flipkart/search/BuyBook.java
@@ -1,11 +1,17 @@
 package com.flipkart.search;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BuyBook {
 	
 	public WebDriver driver = null;
 	public static WebDriverWait wait = null;
+	
+	public void PreConditions() {		
+		driver = new ChromeDriver();
+		
+	}
 
 }
