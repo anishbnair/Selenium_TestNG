4ba3e9bdf527f050a2af0de9de31e86d5fa8b5d3
anishbnair <nbanish@gmail.com>
2018-11-05 16:59:45 -0500

    declared webdriver and wait

--- BookSearch/src/com/flipkart/search/BuyBook.java
+++ BookSearch/src/com/flipkart/search/BuyBook.java
@@ -1,5 +1,11 @@
 package com.flipkart.search;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BuyBook {
+	
+	public WebDriver driver = null;
+	public static WebDriverWait wait = null;
 
 }
