2d0d20d642bc9f37721bb512b195ff6603d946a4
anishbnair <nbanish@gmail.com>
2018-11-05 17:44:56 -0500

    added @BeforeMethod

--- BookSearch/src/com/flipkart/search/BuyBook.java
+++ BookSearch/src/com/flipkart/search/BuyBook.java
@@ -5,20 +5,21 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
 
 public class BuyBook {
 	
 	public WebDriver driver = null;
 	public static WebDriverWait wait = null;
 	
+	@BeforeMethod
 	public void PreConditions() {		
 		driver = new ChromeDriver();
 		wait = new WebDriverWait(driver, 20);
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.get("https://www.flipkart.com/");
-		
+		driver.get("https://www.flipkart.com/");		
 	}
 
 }
