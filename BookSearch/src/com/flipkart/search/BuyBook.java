f901db83638e0894f1c004434c6ad04838aa0139
anishbnair <nbanish@gmail.com>
2018-11-05 20:22:47 -0500

    Added steps to add book to cart and place order

--- BookSearch/src/com/flipkart/search/BuyBook.java
+++ BookSearch/src/com/flipkart/search/BuyBook.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -36,10 +37,30 @@ public class BuyBook {
 		// click on book
 		driver.findElement(By.xpath("//img[@class='_1Nyybr  _30XEf0']")).click();
 		
+		// To get current window	
+		String currentTab = driver.getWindowHandle();
+		for (String tab : driver.getWindowHandles()) {
+		    if (!tab.equals(currentTab)) {
+		        driver.switchTo().window(tab); 
+		    }       
+		}
+		
 		// Enter ZIP code
 		driver.findElement(By.xpath("//input[@id='pincodeInputId']")).sendKeys("685588");
 		// Click on Check
 		driver.findElement(By.xpath("//span[@class='_2aK_gu']")).click();
+		// Capture expected delivery
+		String expectedDelivery = driver.findElement(By.xpath("//span[@class='_3nCwDW']")).getText();
+		Reporter.log(expectedDelivery);
+		System.out.println(expectedDelivery);
+		
+		// Add book to cart
+		driver.findElement(By.xpath("//button[@class='_2AkmmA _2Npkh4 _2MWPVK']")).click();
+		// Place order
+		driver.findElement(By.xpath("//button[@class='_2AkmmA _14O7kc _7UHT_c']")).click();
+		
+//		driver.close();
+//		driver.switchTo().window(currentTab);
 		
 	}
 
